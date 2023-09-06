b027f7f1763fca5c2581b9f3fae45bfb8d56d313
salmanben <salmanbenomar250@gmail.com>
2023-09-05 18:41:42 -0700

    Remove 101-locked_class.py file

--- 0x09-python-everything_is_object/101-locked_class.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-'''A module containing a class with restrictions.
-'''
-
-
-class LockedClass:
-    '''Represents a class with restricted attribute modification.
-    '''
-    __slots__ = ['first_name']
