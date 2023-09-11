4a643d8bfa1e33bf37d5977ea8f8f272973ca416
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2023-09-12 00:01:32 +0100

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -8,11 +8,13 @@ This module defines 'BaseGeometry' class.
 class BaseGeometry:
     """
     This class defines an instance method:
-    def area(self): that raises an Exception with the message area() is not implemented
+    def area(self):that raises an Exception
+    with the message area() is not implemented
     """
     def area(self):
         """
         Raises:
-            Exception: This exception is raised when this method is invoked.
+            Exception: This exception is raised
+            when this method is invoked.
         """
         raise Exception("area() is not implemented")
