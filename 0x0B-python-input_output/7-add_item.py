31d60c06914cd0b63e2dac844ef3dd44fadbc115
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2023-09-12 21:14:31 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,16 +3,16 @@
 A script that adds command-line arguments to a list,
 saves the list as a JSON representation in a file.
 """
-import json
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_modele = __import__('6-load_from_json_file')
+    load_from_json_file = load_modele.load_from_json_file
 
-try:
-    my_list = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    my_list = []
-    
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, 'add_item.json')
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
