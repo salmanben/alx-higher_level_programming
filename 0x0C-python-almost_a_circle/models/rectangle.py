c1d2dae7ee0737015d3b8d5a2afec8e4a77d749d
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2023-09-18 21:42:47 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines 'Rectangle' class."""
 
-Base = __import__("base").Base
+from models.base import Base
 
 class Rectangle(Base):
     """Represents the Rectangle class."""
